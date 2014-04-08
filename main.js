923c66d342eed3d7fed578b0d514bdb20efd8fa3
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-08 08:58:22 -0600

    add state object to QC against

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-info = {
+var info = {
     Phone: {},
     BirthDate: {},
     PostalCode: {},
@@ -6,7 +6,65 @@ info = {
     Married: {}
 };
 
+var states = {
+    AL: 1,
+    AK: 1,
+    AZ: 1,
+    AR: 1,
+    CA: 1,
+    CO: 1,
+    CT: 1,
+    DE: 1,
+    DC: 1,
+    FL: 1,
+    GA: 1,
+    HI: 1,
+    ID: 1,
+    IL: 1,
+    IN: 1,
+    IA: 1,
+    KS: 1,
+    KY: 1,
+    LA: 1,
+    ME: 1,
+    MD: 1,
+    MA: 1,
+    MI: 1,
+    MN: 1,
+    MS: 1,
+    MO: 1,
+    MT: 1,
+    NE: 1,
+    NV: 1,
+    NH: 1,
+    NJ: 1,
+    NM: 1,
+    NY: 1,
+    NC: 1,
+    ND: 1,
+    OH: 1,
+    OK: 1,
+    OR: 1,
+    PA: 1,
+    RI: 1,
+    SC: 1,
+    SD: 1,
+    TN: 1,
+    TX: 1,
+    UT: 1,
+    VT: 1,
+    VA: 1,
+    WA: 1,
+    WV: 1,
+    WI: 1,
+    WY: 1
+}
+
 console.log(info);
+
+var isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
 var promptInfo = function(infoObj, name) {
     var i = false;
@@ -53,15 +111,20 @@ info.PostalCode.correctPostalCode = function(value) {
 }
 
 info.State.correctState = function(value) {
-    if (value.length !== 2) {
-        alert('State must be the two state capital abbreviatin(ex: CO)');
-        return false;
-    } else if (value === value.toUpperCase()) {
+    if (states[value.toUpperCase()] === 1) {
         return true;
     } else {
-        alert('State must be the two state capital abbreviatin(ex: CO)');
         return false;
     }
+    // if (value.length !== 2) {
+    //     alert('State must be the two state capital abbreviatin(ex: CO)');
+    //     return false;
+    // } else if (value === value.toUpperCase()) {
+    //     return true;
+    // } else {
+    //     alert('State must be the two state capital abbreviatin(ex: CO)');
+    //     return false;
+    // }
 }
 
 info.Married.correctMarried = function(value) {
